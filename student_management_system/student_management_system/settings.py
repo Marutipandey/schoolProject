074e7e4d23f9fdb544726d28f1e4a7e4fabd045d
Marutipandey <pp874765@gmail.com>
2022-10-20 14:11:08 +0530

    no

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for student_management_system project.
 
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
